66f730049b133280c14c98d76f788d3df0d758ab
Jacob Crummey <161166895+NyuSD@users.noreply.github.com>
2025-01-23 14:54:45 -0330

    Created password generator - Added generatePassword function to return a random set of characters, length defined by passwordLength

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const process = require('process');
 const args = process.argv.slice(2);
 
+const passwordLength = 8
+/**
+ * Generates a random password of lower case letters only
+ */
+function generatePassword() {
+    const avaliableChars = 'abcdefghijklmnopqrstuvwxyz';
+    let password = '';
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * avaliableChars.length);
+        password += avaliableChars[randomIndex];
+    }
+    return password;
+}
+
+console.log (generatePassword());
