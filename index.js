eddf853544feadc5aecd28857af1a90889887d89
Jacob Crummey <161166895+NyuSD@users.noreply.github.com>
2025-01-23 15:21:00 -0330

    Added help function -Added printHelp to catch errors and print flag options

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const process = require('process');
 const args = process.argv.slice(2);
 
-const passwordLength = 8
+let passwordLength = 8;
 /**
  * Generates a random password of lower case letters only
  */
@@ -15,4 +15,20 @@ function generatePassword() {
     return password;
 }
 
+
+/**
+ * Help message
+ */
+
+function printHelp() {
+    console.log(`
+Usage: QAP1 [options]
+
+Options:
+  --Length [number]        Set the number of characters for the password (default: 8)
+
+Example:
+    QAP1 --Length 12
+  `);
+}
 console.log (generatePassword());
